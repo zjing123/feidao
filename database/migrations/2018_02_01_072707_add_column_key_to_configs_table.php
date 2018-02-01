3b010061c65bc276e68b6791410e78c839408449
liuchuan <xyz8279635@163.com>
2018-02-01 15:36:26 +0800

    update code

--- database/migrations/2018_02_01_072707_add_column_key_to_configs_table.php
+++ database/migrations/2018_02_01_072707_add_column_key_to_configs_table.php
@@ -14,7 +14,7 @@ class AddColumnKeyToConfigsTable extends Migration
     public function up()
     {
         Schema::table('configs', function (Blueprint $table) {
-            $table->string('key', 20)->nullable()->before('value');
+            $table->string('key', 20)->nullable()->after('user_id');
         });
     }
 

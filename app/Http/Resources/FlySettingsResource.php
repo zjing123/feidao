1fe386a72aec3915bf82284ae9e76689035b5e7d
liuchuan <xyz8279635@163.com>
2018-03-13 13:31:28 +0800

    修改资源文件

--- app/Http/Resources/FlySettingsResource.php
+++ app/Http/Resources/FlySettingsResource.php
@@ -14,20 +14,28 @@ class FlySettingsResource extends Resource
     {
         return $this->filterFields(
             array_merge([
-                'id'           => $this->number,
-                'angles'       => $this->angles,
-                'gravity'      => $this->gravity,
-                'power'        => $this->power,
-                'scale'        => $this->scale,
-                'rX'           => $this->rx,
-                'rY'           => $this->ry,
-                'rZ'           => $this->rz,
-                'autoInterVal' => $this->auto_interval,
-                'autoX'        => (float)$this->auto_x,
-                'autoY'        => (float)$this->auto_y,
-                'autoD'        => $this->auto_d,
-                'created_at'   => $this->created_at,
-                'updated_at'   => $this->updated_at
+                'id'            => $this->number,
+                'angles'        => $this->angles,
+                'gravity'       => $this->gravity,
+                'power'         => $this->power,
+                'scale'         => $this->scale,
+                'rX'            => $this->rx,
+                'rY'            => $this->ry,
+                'rZ'            => $this->rz,
+                'autoInterVal'  => $this->auto_interval,
+                'autoX'         => (float)$this->auto_x,
+                'autoY'         => (float)$this->auto_y,
+                'autoD'         => $this->auto_d,
+                'hitPow'        => (float)$this->hit_pow,
+                'autoRewardMul' => (float)$this->auto_reward_mul,
+                'perfectMul'    => (float)$this->perfect_mul,
+                'levelUpPow'    => (float)$this->level_up_pow,
+                'buyDemandMul'  => (float)$this->buy_demand_mul,
+                'buyDemandPow'  => (float)$this->buy_demand_pow,
+                'buyAutoMul'    => (float)$this->buy_auto_mul,
+                'buyAutoPow'    => (float)$this->buy_auto_pow,
+                'created_at'    => $this->created_at,
+                'updated_at'    => $this->updated_at
             ], parent::toArray($request))
         );
     }
